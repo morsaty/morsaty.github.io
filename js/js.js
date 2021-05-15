cace4afe3ee02041b9bbe0313cb5e5aebfd55a00
morsaty <morsu9701@gmail.com>
2021-05-15 13:47:52 +0200

    Responsive/JS/Socials

--- js/js.js
+++ js/js.js
@@ -1,8 +1,21 @@
-const navbar = document.querySelector('nav');
-window.onscroll = () => {
-    if (window.scrollY > 250) {
-        navbar.classList.add('nav-active');
-    } else {
-        navbar.classList.remove('nav-active');
-    }
-};
+/* smooth transition from nav */
+
+$('.js-aboutus').on('click', function () {
+    $('body, html').animate({
+        scrollTop: $('.about_us').offset().top
+    })
+})
+
+$('.js-projects').on('click', function () {
+    $('body, html').animate({
+        scrollTop: $('.projects').offset().top
+    }, 700)
+})
+
+
+$('.js-contact').on('click', function () {
+    $('body, html').animate({
+        scrollTop: $('.contact').offset().top
+    }, 800)
+})
+
